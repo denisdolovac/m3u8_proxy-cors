f3826acb72272e46ab18aec75e4cab4f73e1aef0
denisdolovac <denis@dolovac.com>
2024-09-08 14:03:12 +0200

    test mp4

--- cors.py
+++ cors.py
@@ -47,7 +47,7 @@ async def cors(request: Request, origins, method="GET") -> Response:
     for key in del_keys:
         headers.pop(key, None)
 
-    if (file_type == "m3u8" or ".m3u8" in url) and code != 404:
+    if (file_type == "m3u8" or ".m3u8" or "mp4" or ".mp4" in url) and code != 404:
         content = content.decode("utf-8")
         new_content = ""
         for line in content.split("\n"):
